a752621c304814b439e4b649bb404e642f720502
Strawberry0121 <kokihigashi0121@icloud.com>
2026-04-14 11:12:15 +0900

    update

--- src/main/java/com/example/shopping/controller/AuthController.java
+++ src/main/java/com/example/shopping/controller/AuthController.java
@@ -4,7 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-
+import com.example.shopping.repository.UserRepository;
 import com.example.shopping.model.User;
 import com.example.shopping.service.UserService;
 
