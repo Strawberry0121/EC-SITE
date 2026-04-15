e15c40fd3f581aed16fb11509dcc4a5b14be9a9d
Strawberry0121 <kokihigashi0121@icloud.com>
2026-04-15 12:02:38 +0900

    update

--- src/main/java/com/example/shopping/config/SecurityConfig.java
+++ src/main/java/com/example/shopping/config/SecurityConfig.java
@@ -32,8 +32,6 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 
         http
-            .csrf(csrf -> csrf.disable()
-            )
             .authenticationProvider(authenticationProvider()
             )
             .authorizeHttpRequests(auth -> auth
