034edd3e742a23dbd9c754615dd4a32dc500ad1a
Strawberry0121 <kokihigashi0121@icloud.com>
2026-04-16 12:23:35 +0900

    update

--- src/main/java/com/example/shopping/model/Product.java
+++ src/main/java/com/example/shopping/model/Product.java
@@ -59,6 +59,9 @@ public class Product {
 
     public double getPrice() { return price; }
     public void setPrice(double price) { this.price = price; }
+    public int getPriceInt() {
+    return (int) price;
+}
 
     public String getDescription() { return description; }
     public void setDescription(String description) { this.description = description; }
