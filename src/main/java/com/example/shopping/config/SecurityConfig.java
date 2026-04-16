a090619c181ef3a12a553405090a5c45bb149c58
Strawberry0121 <kokihigashi0121@icloud.com>
2026-04-16 16:11:18 +0900

    update

--- src/main/java/com/example/shopping/config/SecurityConfig.java
+++ src/main/java/com/example/shopping/config/SecurityConfig.java
@@ -46,6 +46,7 @@ public class SecurityConfig {
             )
 
             .logout(logout -> logout
+                .logoutUrl("/logout")
                 .logoutSuccessUrl("/login?logout")
                 .permitAll()
             );
